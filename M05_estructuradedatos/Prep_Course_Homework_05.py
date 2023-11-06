19dc25e9bba2b46c9d0d7b3daeeac7973915b07d
Matias Ames <matiames71@gmail.com>
2023-11-05 22:22:22 -0300

    Actualización

--- M05_estructuradedatos/Prep_Course_Homework_05.py
+++ M05_estructuradedatos/Prep_Course_Homework_05.py
@@ -7,20 +7,26 @@
 
 # In[3]:
 
-
+lista = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+print(lista)
+type(lista)
 
 
 # 2) Imprimir por pantalla el segundo elemento de la lista
 
 # In[4]:
 
-
+lista = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+segundo_elemento = lista[1]
+print(segundo_elemento)
 
 
 # 3) Imprimir por pantalla del segundo al cuarto elemento
 
 # In[8]:
-
+lista = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+filtrado = lista[1:5]
+print(filtrado)
 
 
 
@@ -28,7 +34,8 @@
 # 4) Visualizar el tipo de dato de la lista
 
 # In[12]:
-
+lista = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+type(lista)
 
 
 
@@ -37,7 +44,9 @@
 
 # In[14]:
 
-
+lista = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+filtrado2 = lista[2:]
+print(filtrado2)
 
 
 
@@ -45,7 +54,9 @@
 
 # In[15]:
 
-
+lista = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+filtrado3 = lista[0:4]
+print(filtrado3)
 
     
 
@@ -54,7 +65,9 @@
 
 # In[16]:
 
-
+lista = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+lista.append('Chicago')
+print(lista)
 
 
 
@@ -66,7 +79,9 @@
 
 # In[20]:
 
-
+lista = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+lista.insert(3, 'Tokio')
+print(lista)
 
 
 
@@ -79,13 +94,18 @@
 
 # In[22]:
 
+lista = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+continenetes = ['Europa','Latino America','Europa','Europa','Latino America']
 
+lista.extend(continenetes)
+print(lista)
 
 
 # 10) Encontrar el índice de la ciudad que en el punto 7 agregamos duplicada. ¿Se nota alguna particularidad?
 
 # In[23]:
-
+lista =['Madrid', 'Cordoba', 'Lisboa', 'Londres', 'Acapulco', 'Chicago']
+lista.index('Chicago')
 
 
 
@@ -94,7 +114,10 @@
 
 # In[24]:
 
+# Si buscas un elemento que no existe arrojaria error: 
 
+lista = ['Madrid', 'Cordoba', 'Lisboa', 'Londres', 'Acapulco', 'Chicago']
+lista.index('Villa Carlos Paz')
 
 
 
@@ -103,14 +126,18 @@
 # In[25]:
 
 
-
+lista = ['Madrid', 'Cordoba', 'Lisboa', 'Londres', 'Acapulco', 'Chicago']
+lista.pop(0)
 
 
 # 13) ¿Qué pasa si el elemento a eliminar no existe?
 
 # In[27]:
 
+# No permite eliminarlo y da error.
 
+lista = ['Madrid', 'Cordoba', 'Lisboa', 'Londres', 'Acapulco', 'Chicago']
+lista.pop(8)
 
 
 
@@ -118,7 +145,10 @@
 
 # In[28]:
 
-
+lista = ['Madrid', 'Cordoba', 'Lisboa', 'Londres', 'Acapulco', 'Chicago']
+lista_sin_chicago = lista.pop(5)
+print(lista_sin_chicago)
+print(lista)
 
 
 
@@ -126,13 +156,15 @@
 
 # In[29]:
 
-
+lista = ['Madrid', 'Cordoba', 'Lisboa', 'Londres', 'Acapulco', 'Chicago']
+print(lista * 4)
 
 
 # 16) Crear una tupla que contenga los números enteros del 1 al 20
 
 # In[32]:
-
+tupla1 = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,)
+type(tupla1)
 
 
 
@@ -140,22 +172,32 @@
 
 # In[35]:
 
-
+tupla1 = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,)
+print(tupla1[10:15])
 
 
 # 18) Evaluar si los números 20 y 30 están dentro de la tupla
 
 # In[41]:
 
-
-
+20 in tupla1
+30 in tupla1
 
 
 # 19) Con la lista creada en el punto 1, validar la existencia del elemento 'París' y si no existe, agregarlo. Utilizar una variable e informar lo sucedido.
 
 # In[48]:
 
+lista = ['Madrid', 'Cordoba', 'Lisboa', 'Londres', 'Acapulco', 'Chicago']
 
+
+var_ciudad_faltante = 'París'
+
+if(not var_ciudad_faltante in lista):
+    lista.append(var_ciudad_faltante)
+    print(lista)
+else:
+    print('Ya estaba la ciudad en la lista')
 
 
 
@@ -164,14 +206,15 @@
 # In[51]:
 
 
-
+print(lista.count('Cordoba'))
+print(tupla1.count(15))
 
 
 # 21) Convertir la tupla en una lista
 
 # In[52]:
 
-
+list(tupla1)
 
 
 
@@ -180,7 +223,13 @@
 # In[55]:
 
 
-
+tupla1 = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,)
+elemento0 = tupla1[0]
+elemento1 = tupla1[1]
+elemento2 = tupla1[2]
+print(elemento0)
+print(elemento1)
+print(elemento2)
 
 
 # 23) Crear un diccionario utilizando la lista crada en el punto 1, asignandole la clave "ciudad". Agregar tambien otras claves, como puede ser "Pais" y "Continente".
@@ -188,14 +237,18 @@
 # In[57]:
 
 
+lista_ciudades = ['Madrid','Cordoba','Lisboa','Londres','Acapulco']
+Paises = ['España', 'Argentina','Portugal', 'Inglaterra','Mexico']
 
+diccionario = {'Ciudades': lista_ciudades, 'Paises': Paises}
 
 
 
 # 24) Imprimir las claves del diccionario
 
 # In[59]:
-
+claves_lista = list(diccionario.keys())
+print(claves_lista)
 
 
 
@@ -203,6 +256,9 @@
 
 # In[61]:
 
-
+diccionario = {'Ciudades': ['Madrid','Cordoba','Lisboa','Londres','Acapulco'],
+               'Paises': ['España', 'Argentina','Portugal', 'Inglaterra','Mexico']}
+ciudades = diccionario['Ciudades']
+print(ciudades)
 
 
